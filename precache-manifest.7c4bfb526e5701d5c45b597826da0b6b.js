3acf5072a1a81e25124f26b6080cf9d07bda73a5
Marc <mwgdavies@gmail.com>
2021-01-11 15:20:20 +0100

    Updates

---
+++ precache-manifest.7c4bfb526e5701d5c45b597826da0b6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2121117d05ab30cd54b2dad8884f60ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d3458dead729517177b",
+    "url": "/static/css/2.1222e6d6.chunk.css"
+  },
+  {
+    "revision": "64b28cb6d00ec65604cd",
+    "url": "/static/css/main.2f1329b8.chunk.css"
+  },
+  {
+    "revision": "3d3458dead729517177b",
+    "url": "/static/js/2.1ae169a5.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/static/js/2.1ae169a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64b28cb6d00ec65604cd",
+    "url": "/static/js/main.6589b086.chunk.js"
+  },
+  {
+    "revision": "48d729ce2b874e04a323",
+    "url": "/static/js/runtime-main.fb9a094a.js"
+  }
+]);
